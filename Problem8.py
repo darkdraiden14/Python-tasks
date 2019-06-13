ad3543741cdcb51ebdb66da4e3b939c3a752e933
darkdraiden14 <thesauravrocks@gmail.com>
2019-06-13 11:35:39 +0530

    Take shell command as input,give output if exists,else generate error message

---
+++ Problem8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import os
+import shutil
+
+#Creating file in order to store commands
+file=open("ListOfCommand","a")
+#Take commands as a input from User
+cmmnd=input("Enter the Shell Comand:")
+file.write(cmmnd)
+
+#Execute the Command
+if(shutil.which(cmmnd)):
+    print("Command Founnd , Output of given Command is :\n")
+    os.system(cmmnd)
+
+else:
+    print("\nThe Command you have entered Does Not exist")
+file.close()
+
+
+#To read the Commands entered by User
+file1=open("ListOfCommand","r")
+
+print("\nYou have entered following commands So far:\n")
+print(file1.read())
+
+file1.close()
