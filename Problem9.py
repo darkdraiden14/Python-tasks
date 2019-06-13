6904e56d3472b8a7af3dfbc7d04fab0c52ccca8f
darkdraiden14 <thesauravrocks@gmail.com>
2019-06-14 01:01:48 +0530

    take upto 500 characters as input and perform some operations

--- Problem9.py
+++ Problem9.py
@@ -1,1 +1,18 @@
-put upto 500 Characters and perform some given Operations
+#To Take input from user
+ch = input("Enter any thing:")
+
+#if the length exceeds the limit 
+if len(ch) > 500:
+    ch=ch[:500]
+
+#making dictionary and here cntForCh stands for (Count For Ch) variable
+dictCount={}
+
+for c in ch:
+    if c in dictCount.keys():
+        dictCount[c] += 1
+    else :
+        dictCount[c] = 1
+
+list1=list(dictCount.values())
+list1.sort(reverse=True)
