6634505e82e5586d3a8116db856de898406f064f
darkdraiden14 <thesauravrocks@gmail.com>
2019-06-13 10:39:54 +0530

    This python program will perform operation of shell command like cat,cat -b,cat -s,cat -E

--- Problem6.py
+++ Problem6.py
@@ -2,31 +2,36 @@
 
 choice = int(input("Enter you Choice : \n 1. Cat \t2.Cat -b\t3.Cat -s\t4.Cat -E\n:"))
 
-
+#OPEN the FILE
+file=open("demofile.txt","r")
 #Normal Reading like cat function
 if choice==1:
-    file=open("demofile.txt","r")
     print(file.read())
-    file.close()
 
 #With -b option
 elif choice==2:
     i=1
-    file=open("demofile.txt","r")
-    #to Store lines of a file in a list
+    #to print non empty lines
     for lines in file:
         if lines.strip() !="":
             print(str(i)+". "  +lines.strip())
             i+=1
-    file.close()
 
 #With -s option
 elif choice==3:
-    file=open("demofile.txt","r")
-    #to Store lines of a file in a list
+    #to suppress empty lines
     for lines in file:
         if lines.strip() !="":
             print(lines.strip())
-    file.close()
 
 #with -E option:
+elif choice==4:
+    #to put $ symbol in end of files
+    for lines in file:
+        print(lines.strip() +"$")
+
+#if wrong choice
+else:
+    print("Enter choice from given inputs")
+
+file.close()
