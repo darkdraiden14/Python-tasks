2a53e9a67d703b6772be7c5cda2956bf5fec0870
darkdraiden14 <thesauravrocks@gmail.com>
2019-06-13 10:02:41 +0530

    This python program will perform operation similar to cat and cat -b shell commands

--- Problem6.py
+++ Problem6.py
@@ -1,3 +1,20 @@
+#Python Program which performs similar operation like "CAT" shell command
+
+choice = int(input("Enter you Choice : \n 1. Cat \t2.Cat -b\t3.Cat -s\t4.Cat -E\n:"))
+
+
 #Normal Reading like cat function
-file = open("demofile.txt","r")
-print(file.read())
+if choice==1:
+    file=open("demofile.txt","r")
+    print(file.read())
+    file.close()
+
+#With -b option
+elif choice==2:
+    i=1
+    file=open("demofile.txt","r")
+    #to Store lines of a file in a list
+    for lines in file:
+        if lines.strip() !="":
+            print(str(i) +lines.strip())
+            i+=1
