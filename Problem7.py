7eb07d705ebadc07e0297818c6051613cde45911
darkdraiden14 <thesauravrocks@gmail.com>
2019-06-14 22:42:21 +0530

    Program which performs operation similar to Touch

---
+++ Problem7.py
@@ -0,0 +1,24 @@
+choice=input("Enter your choice : 1)touch (create single file)\n 2)touch (create multiple files)\n 3) touch -c 4) touch -a\n 5)touch -m")
+
+if choice=='1':
+	files=input("Enter File Name : ")	
+	f=open(files ,'w')
+	f.close()
+	print("You have successfully created a File")
+
+elif choice=='2':
+	num=int(input("Number of files you want to create :"))
+	listOfFile=[]
+	for i in range(num):
+		listOfFile.append(input(f"Enter file name {i}: "))
+	for files in listOfFile:
+		f=open(files,'w')
+		f.close()
+	print("You have successfully created a File")
+
+elif choice=='3':
+	files=input("enter file name : ")
+	print("Nothing happened")
+	exit()
+else:
+	print("Choose valid INPUT")
