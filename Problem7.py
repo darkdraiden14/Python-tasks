721cd0365744d7204c5a1d77d2725c6e1bca3240
darkdraiden14 <thesauravrocks@gmail.com>
2019-06-14 22:46:21 +0530

    Python program to perform operation similar to normal shell TOUCH command

--- Problem7.py
+++ Problem7.py
@@ -1,11 +1,13 @@
 choice=input("Enter your choice : 1)touch (create single file)\n 2)touch (create multiple files)\n 3) touch -c 4) touch -a\n 5)touch -m")
 
+#Touch single file
 if choice=='1':
 	files=input("Enter File Name : ")	
 	f=open(files ,'w')
 	f.close()
 	print("You have successfully created a File")
 
+#Touch multiple file
 elif choice=='2':
 	num=int(input("Number of files you want to create :"))
 	listOfFile=[]
@@ -16,9 +18,12 @@ elif choice=='2':
 		f.close()
 	print("You have successfully created a File")
 
+#touch -c which means it does not create any file
 elif choice=='3':
 	files=input("enter file name : ")
 	print("Nothing happened")
 	exit()
 else:
 	print("Choose valid INPUT")
+
+#More to come
